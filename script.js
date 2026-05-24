c657ca244fe8a7c95415288520c98a6e66404609
Stanley <stanley44u@gmail.com>
2026-05-24 01:02:10 +0000

    core calculator logic complete!

--- script.js
+++ script.js
@@ -22,6 +22,9 @@ let operator = "";
 let secondNumber = "";
 
 function operate(operator, a, b) {
+  a = parseFloat(a);
+  b = parseFloat(b);
+
   if (operator === "+") {
     return add(a, b);
 }
@@ -38,3 +41,47 @@ function operate(operator, a, b) {
     return divide(a, b);
   }
 }
+
+const buttons = document.querySelectorAll(".btn");
+const display = document.querySelector("#display");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector("#equals");
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const buttonValue = button.dataset.value;
+
+    if (buttonValue !== undefined) {
+      display.value += buttonValue;
+    }
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+
+    firstNumber = display.value;
+    operator = button.dataset.value;
+    display.value = "";
+  });
+});
+
+equalsButton.addEventListener("click", () => {
+
+  secondNumber = display.value;
+  const result = operate(operator, firstNumber, secondNumber);
+  display.value = result;
+});
+
+clearButton.addEventListener("click", () => {
+  firstNumber = "";
+  operator = "";
+  secondNumber = "";
+  display.value = "";
+});
+
+deleteButton.addEventListener("click", () => {
+  display.value = display.value.slice(0, -1);
+});
